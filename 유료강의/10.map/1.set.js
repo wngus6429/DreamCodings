49ec5f472fcfd0a84b97d160d65696ee7a36e0ed
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-04-16 02:32:26 +0900

    Update 1.set.js

--- 유료강의/10.map/1.set.js
+++ 유료강의/10.map/1.set.js
@@ -1,46 +1,49 @@
-// Set
+//! Set
 const set = new Set([1, 2, 3]);
 console.log(set);
 
-// 사이즈 확인
-console.log(set.size);
+//! 사이즈 확인
+console.log('사이즈', set.size);
 
-// 존재하는지 확인
-console.log(set.has(2));
-console.log(set.has(6));
+//! 존재하는지 확인
+console.log('존재', set.has(2));
+console.log('존재2', set.has(6));
 
-// 순회
+//! 순회
+console.log('순회시작');
 set.forEach((item) => console.log(item));
 for (const value of set.values()) {
   console.log(value);
 }
+console.log('순회끝');
 
-// 추가
+//! 추가
 set.add(6);
 console.log(set);
 set.add(6);
-console.log(set);
+console.log('무시당함', set);
 
-// 삭제
+//! 삭제
 set.delete(6);
 console.log(set);
 
-// 전부 삭제
+//! 전부 삭제
 set.clear();
 console.log(set);
 
-// 오브젝트 세트
+//! 오브젝트 세트
 const obj1 = { name: '🍎', price: 8 };
 const obj2 = { name: '🍌', price: 5 };
 const objs = new Set([obj1, obj2]);
-console.log(objs);
+console.log('오브젝트세트', objs);
 
-// 퀴즈
+//! 퀴즈
 obj1.price = 10;
 objs.add(obj1);
 console.log(objs);
 
-// 퀴즈
+console.log('끝');
+//! 퀴즈2
 const obj3 = { name: '🍌', price: 5 };
 objs.add(obj3);
 console.log(objs);
