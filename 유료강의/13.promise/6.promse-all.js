a2ddc658145a1ffc7cd0d5b3e458bc3c6028bf5b
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-04-22 02:40:58 +0900

    async

--- 유료강의/13.promise/6.promse-all.js
+++ 유료강의/13.promise/6.promse-all.js
@@ -15,20 +15,17 @@ function getApple() {
 }
 
 // 바나나과 사과를 같이 가지고 오기
-getBanana() //
-  .then((banana) =>
-    getApple() //
-      .then((apple) => {
-        return [banana, apple];
-      })
-  )
-  .then(console.log);
-//결과가 4초후에 나옴[ '🍌', '🍎' ]
+// getBanana() //
+//   .then((banana) =>
+//     getApple() //
+//       .then((apple) => {
+//         return [banana, apple];
+//       })
+//   )
+//   .then(console.log);
+//! 결과가 4초후에 나옴[ '🍌', '🍎' ]
 
-// function getOrange() {
-//   return Promise.reject(new Error('no orange'));
-// }
-
+//! 축약버전
 // getBanana() //
 //   .then((banana) =>
 //     getApple() //
@@ -36,18 +33,35 @@ getBanana() //
 //   )
 //   .then(console.log);
 
-// Promise.all 병렬적으로 한번에 모든 Promise들을 실행!
-// Promise.all([getBanana(), getApple()]) //
-//   .then((fruits) => console.log('all', fruits));
+// 위에 저거 총 4초 걸리는데 병렬처리 하면 빨리 가능
+function getOrange() {
+  return Promise.reject(new Error('no orange'));
+}
 
-// // Promise.race 주어진 Promise중에 제일 빨리 수행된것이 이김!
-// Promise.race([getBanana(), getApple()]) //
-//   .then((fruit) => console.log('race', fruit));
+getBanana() //
+  .then((banana) =>
+    getApple() //
+      .then((apple) => [banana, apple])
+  )
+  .then(console.log);
 
-// Promise.all([getBanana(), getApple(), getOrange()]) //
-//   .then((fruits) => console.log('all-error', fruits))
-//   .catch(console.log);
+//! Promise.all 병렬적으로 한번에 모든 Promise들을 실행!
+//! 위에는 4초 걸렸지만 여기서는 1초와 3초이기 때문에 3초가 걸림
+//! 성공 했을때만 all
+Promise.all([getBanana(), getApple()]) //
+  .then((fruits) => console.log('all', fruits));
 
-// Promise.allSettled([getBanana(), getApple(), getOrange()]) //
-//   .then((fruits) => console.log('all-settle', fruits))
-//   .catch(console.log);
+//! Promise.race 주어진 Promise중에 제일 빨리 수행된것이 이김!
+Promise.race([getBanana(), getApple()]) //
+  .then((fruit) => console.log('race', fruit));
+
+//! 당연히 에러 뜬다
+//! 에러 뜨니까 catch 해주는게 좋음, 다 성공해야 then으로 감
+Promise.all([getBanana(), getApple(), getOrange()]) //
+  .then((fruits) => console.log('all-error', fruits))
+  .catch(console.log);
+//! 에러가 발생한거랑, 성공한거랑 다 받아보고 싶을때
+//! allSettled
+Promise.allSettled([getBanana(), getApple(), getOrange()]) //
+  .then((fruits) => console.log('all-settle', fruits))
+  .catch(console.log);
--- 유료강의/13.promise/7.async.js
+++ 유료강의/13.promise/7.async.js
@@ -18,11 +18,11 @@ function getOrange() {
   return Promise.reject(new Error('no orange'));
 }
 
-// 바나나과 사과를 같이 가지고 오기
+//! 바나나과 사과를 같이 가지고 오기, 4초걸림
 async function fetchFruits() {
-  const banana = await getBanana();
-  const apple = await getApple();
-  return [banana, apple];
+  const banana = await getBanana(); //resolve값 가져옴
+  const apple = await getApple(); //resolve값 가져옴
+  return [banana, apple]; //resolve가 생략되어 있는거지
 }
 
 fetchFruits() //
