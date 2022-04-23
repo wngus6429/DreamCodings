4e2330b06ef6e07d3abbb8ac2043b8ba95edd1d5
ParkJuhyun <wngus6429@gmail.com>
2022-04-23 18:49:42 +0900

    Fetch, scope, Gc, hoisting, var, strict

--- 유료강의/14.scope/2.scope-quiz.js
+++ 유료강의/14.scope/2.scope-quiz.js
@@ -2,17 +2,21 @@
   const x = 1;
   {
     const y = 2;
-    console.log(x);
+    console.log(x); //! 1이 출력됨
+    //! 블럭 내부에서 밖에는 참조 가능함
   }
   console.log(x);
-  // console.log(y);
+  //! console.log(y); //블럭 밖에서 내부는 참조 불가
 }
 
-const text = 'global'; // 전역 변수, 전역 스코프 (글로벌 변수, 글로벌 스코프)
+//! 전역 변수, 전역 스코프 (글로벌 변수, 글로벌 스코프)
+const text = 'global';
 {
-  const text = 'inside block1'; // 지역 변수(로컬변수), 지역 스코프(로컬스코프)
+  //! 지역 변수(로컬변수), 지역 스코프(로컬스코프)
+  const text = 'inside block1';
   {
     const text = 'inside block2';
+    //!외부 한칸씩 사면서 가까운 text를 찾음
     console.log(text);
   }
 }
--- 유료강의/14.scope/6.strict.js
+++ 유료강의/14.scope/6.strict.js
@@ -1,16 +1,19 @@
 'use strict';
-// 엄격 모드 strict mode
-// 리액트와 같은 프레임워트 사용시 기본적으로 엄격 모드임
-// var x = 1;
-// delete x;
+//! 엄격 모드 strict mode
+// !리액트와 같은 프레임워트 사용시 기본적으로 엄격 모드임
+//? var x = 1;
+//? delete x; // 이렇게 변수 삭제하는거 안됨
 
+//! use strict하면 키워드 앞에 꼭 붙여야함
 function add(x) {
+  'use strict'; //! 이렇게 함수 안에서만 사용 가능함
   var a = 2;
   var b = a + x;
   console.log(this);
 }
 add(1);
 
+//! 엄격모드에서는 num 앞에 키워드 붙여야함
 const array = [1, 2, 3];
 for (const num of array) {
   console.log(num);
