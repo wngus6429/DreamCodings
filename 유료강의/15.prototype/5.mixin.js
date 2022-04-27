ba75080f9f01047b8de57c89d1bd67cbf706f323
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-04-28 02:02:17 +0900

    update

--- 유료강의/15.prototype/5.mixin.js
+++ 유료강의/15.prototype/5.mixin.js
@@ -19,10 +19,12 @@ function Dog(name) {
 
 Object.assign(Dog.prototype, play, sleep);
 const dog = new Dog('멍멍');
-console.log(dog);
-dog.play();
-dog.sleep();
+console.log(dog); // Dog { name: '멍멍' }
+dog.play(); // 멍멍 놀아요!
+dog.sleep(); // 멍멍 자요!
 
+// 자스의 클래스는 뭐다? 클래스처럼 보이지만 내부는 프로토타입을 베이스로함
+// 자스는 원래 본질, 프로토타입을 베이스로한 언어니까
 class Animal {}
 class Tiger extends Animal {
   constructor(name) {
@@ -30,8 +32,9 @@ class Tiger extends Animal {
     this.name = name;
   }
 }
-
+// 그래서 위에꺼와 같이 비슷하게 할수 있다.
 Object.assign(Tiger.prototype, play, sleep);
 const tiger = new Tiger('어흥!');
-tiger.play();
-tiger.sleep();
+console.log(tiger); // Tiger { name: '어흥!' }
+tiger.play(); //어흥! 놀아요!
+tiger.sleep(); //어흥! 자요!
