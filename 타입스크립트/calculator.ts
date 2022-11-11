7fee202288c19d7e2e30abd47c5684024399ae88
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-11-12 03:24:20 +0900

    update typescript

--- 타입스크립트/calculator.ts
+++ 타입스크립트/calculator.ts
@@ -1,8 +1,44 @@
 /**
  * Let's make a calculator 🧮
  */
+function calculate(what: string, num1: number, num2: number): number {
+  if (what === 'add') {
+    return num1 + num2;
+  } else if (what === 'substract') {
+    return num1 - num2;
+  } else if (what === 'multiply') {
+    return num1 * num2;
+  } else if (what === 'divide') {
+    return num1 / num2;
+  } else {
+    return num1 % num2;
+  }
+}
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
+//! ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate2(what: Command, num1: number, num2: number): number {
+  switch (what) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw new Error('unkonwn command');
+  }
+}
+console.log(calculate2('add', 1, 3)); // 4
+console.log(calculate2('substract', 3, 1)); // 2
+console.log(calculate2('multiply', 4, 2)); // 8
+console.log(calculate2('divide', 4, 2)); // 2
+console.log(calculate2('remainder', 5, 2)); // 1
--- 타입스크립트/game.ts
+++ 타입스크립트/game.ts
@@ -1,6 +1,27 @@
 /**
  * Let's make a game 🕹
  */
+let position = { x: 0, y: 0 };
+function move(direction: string): object {
+  let po = { x: 0, y: 0 };
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error('unkonwn command');
+  }
+  return po;
+}
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
