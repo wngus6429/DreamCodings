b390e7d6ed61f1e2a86971b51041bdc026475b9d
ParkJuhyun <wngus6429@gmail.com>
2023-03-18 18:51:17 +0900

    update typescript study 다형성, 상속 0318

--- 타입스크립트/객체지향/3-7. composition.ts
+++ 타입스크립트/객체지향/3-7. composition.ts
@@ -1,5 +1,5 @@
 {
-    type CoffeeCup = { shots: number; hasMilk: boolean;}
+    type CoffeeCup = { shots: number; hasMilk?: boolean; hasSugar?: boolean };
 
     interface CoffeeMaker { makeCoffee(shots: number): CoffeeCup; }
     
@@ -42,6 +42,7 @@
         }
     }
 
+    // 커피머신을 상속한 클래스
     class CaffeLatteMachine extends CoffeeMachine{
         constructor(beans:number, public readonly serialNumber:string){
             super(beans);
@@ -54,9 +55,37 @@
             return { ...coffee, hasMilk: true }
         }
     }
-    const machine = new CoffeeMachine(23);
-    const latteMachine = new CaffeLatteMachine(23, "ssss");
-    const coffee = latteMachine.makeCoffee(1);
-    console.log('커피임', coffee);
-    console.log(latteMachine.serialNumber)
+    
+    // 커피머신을 상속한 클래스
+    class SweetCoffeeMaker extends CoffeeMachine{
+        makeCoffee(shots: number): CoffeeCup{
+            const coffee = super.makeCoffee(shots);
+            return { ...coffee, hasSugar: true }
+        }
+    }
+
+    // 다형성의 장점, 내부적으로 구현된 다양한 클래스들이 한가지 
+    // 인터페이스를 구현하거나 또는 동일한 부모클래스를 상속했을때 
+    // 이렇게 동일한 함수를 어떤 클래스인지 구분하지 않고 공통된 API를 부를수 있는게 장점이다. 
+    const machines: CoffeeMaker[] = [
+        new CoffeeMachine(10),
+        new CaffeLatteMachine(16, '1'),
+        new SweetCoffeeMaker(20),
+        new CoffeeMachine(10),
+        new CaffeLatteMachine(16, '1'),
+        new SweetCoffeeMaker(20),
+    ]
+    // 다형성이란 하나의 인터페이스나 또는 부모의 클래스를 상속한 자식 
+    // 클래스들이 인터페이스와 부모클래스에 있는 함수들을 다른 방식으로
+    // 다양하게 구성함으로서 조금 더 다양성을 만들수 있는걸 말한다.
+    machines.forEach(machine => {
+        console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+        machine.makeCoffee(1);
+    })
+
+    // const machine = new CoffeeMachine(23);
+    // const latteMachine = new CaffeLatteMachine(23, "ssss");
+    // const coffee = latteMachine.makeCoffee(1);
+    // console.log('커피임', coffee);
+    // console.log(latteMachine.serialNumber)
 }
