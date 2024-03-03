2c4714fb6127c27fae6e255e3e3568ca515e15e9
ParkJuhyun <wngus6429@gmail.com>
2024-03-03 20:06:19 +0900

    Update page.tsx

--- NextBlog/src/app/page.tsx
+++ NextBlog/src/app/page.tsx
@@ -1,13 +1,13 @@
-import CarouselPosts from '@/components/CarouselPosts';
-import FeaturedPosts from '@/components/FeaturedPosts';
-import Hero from '@/components/Hero';
-import Image from 'next/image';
+import CarouselPosts from "@/components/CarouselPosts";
+import FeaturedPosts from "@/components/FeaturedPosts";
+import Hero from "@/components/Hero";
+
 //! 이곳이 결국은 children임
 export default function Home() {
   return (
     <>
       <Hero />
-      <section className='bg-gray-500'>Main</section>
+      <section className="bg-gray-500">Main</section>
       {/* @ts-expect-error Async Server Component */}
       <FeaturedPosts />
       {/* @ts-expect-error Async Server Component */}
