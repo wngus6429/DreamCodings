dabeac00ca5f2204fd864cf505c00f6f96ad73e0
ParkJuhyun <wngus6429@gmail.com>
2024-03-19 02:44:39 +0900

    Update page.tsx

--- NextBlog/src/app/posts/page.tsx
+++ NextBlog/src/app/posts/page.tsx
@@ -3,7 +3,9 @@ import { getAllPosts } from "@/service/posts";
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
+  console.log('filter', posts.filter((post) => post.featured));
+  console.log('posts', posts.map((post) => post.featured));
   const categories = [...new Set(posts.map((post) => post.category))];
-  console.log(categories);
+  console.log('categories', categories);
   return <FilterablePosts posts={posts} categories={categories} />;
 }
