23f7112089dd73dea8cfcfbca01e83891db14b96
ParkJuhyun <wngus6429@gmail.com>
2024-03-31 17:17:40 +0900

    Email, Yup

--- NextBlog/src/components/ContactForm.tsx
+++ NextBlog/src/components/ContactForm.tsx
@@ -1,6 +1,7 @@
-'use client';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import Banner, { BannerData } from './Banner';
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Banner, { BannerData } from "./Banner";
+import { sendContactEmail } from "@/service/contact";
 
 type Form = {
   from: string;
@@ -8,12 +9,13 @@ type Form = {
   message: string;
 };
 
+const DEFAULT_DATA = {
+  from: "",
+  subject: "",
+  message: "",
+};
 export default function ContactForm() {
-  const [form, setForm] = useState<Form>({
-    from: '',
-    subject: '',
-    message: '',
-  });
+  const [form, setForm] = useState<Form>(DEFAULT_DATA);
   const [banner, setBanner] = useState<BannerData | null>(null);
 
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -23,30 +25,69 @@ export default function ContactForm() {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
-    setBanner({ message: '성공했어!!', state: 'success' });
-    setTimeout(() => {
-      setBanner(null);
-    }, 3000);
+    sendContactEmail(form) //
+      .then(() => {
+        setBanner({ message: "메일을 성공적으로 보냄", state: "success" });
+        setForm(DEFAULT_DATA);
+      })
+      .catch(() => {
+        setBanner({ message: "메일 보내기 실패함", state: "error" });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setBanner(null);
+        }, 3000);
+      });
   };
 
   return (
-    <section className='w-full max-w-md'>
+    <section className="w-full max-w-md">
       {banner && <Banner banner={banner} />}
-      <form onSubmit={onSubmit} className='w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white'>
-        <label htmlFor='from' className='font-semibold'>
+      <form
+        onSubmit={onSubmit}
+        className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white"
+      >
+        <label htmlFor="from" className="font-semibold">
           Your Email
         </label>
-        <input type='email' id='from' name='from' required autoFocus value={form.from} onChange={onChange} className='text-black'/>
-        <label htmlFor='subject' className='font-semibold'>
+        <input
+          type="email"
+          id="from"
+          name="from"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+          className="text-black"
+        />
+        <label htmlFor="subject" className="font-semibold">
           Subject
         </label>
-        <input type='text' id='subject' name='subject' required value={form.subject} onChange={onChange} className='text-black'/>
-        <label htmlFor='message' className='font-semibold'>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={form.subject}
+          onChange={onChange}
+          className="text-black"
+        />
+        <label htmlFor="message" className="font-semibold">
           Message
         </label>
-        <textarea rows={10} id='message' name='message' required value={form.message} onChange={onChange} className='text-black' />
-        <button type="submit" className='bg-yellow-300 text-black font-bold hover:bg-yellow-400'>
+        <textarea
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+          className="text-black"
+        />
+        <button
+          type="submit"
+          className="bg-yellow-300 text-black font-bold hover:bg-yellow-400"
+        >
           Submit
         </button>
       </form>
