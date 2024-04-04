429a6b7a8c9339e73ed69f984a4554115f9c1b9d
Park Juhyun <wngus6429@gmail.com>
2024-04-05 02:43:28 +0900

    타입스크립트

--- 타입스크립트/10. javascript/10-1-proto.js
+++ 타입스크립트/10. javascript/10-1-proto.js
@@ -1,7 +1,8 @@
 const x = {};
 const y = {};
 console.log(x);
-console.log(y.toString());
+console.log(y);
+console.log(x.toString());
 console.log(x.__proto__ === y.__proto__);
 
 const array = [];
@@ -9,16 +10,27 @@ console.log(array);
 
 console.clear();
 
-function CoffeeMachine(beans){
-    this.beans = beans
-    this.makeCoffee = (shots) => {
-        console.log('making...')
-    }
+function CoffeeMachine(beans) {
+  this.beans = beans;
+  // Instance member level
+  // this.makeCoffee = (shots) => {
+  //   console.log("making... ☕️");
+  // };
 }
-CoffeeMachine.prototype.makeCoffee = function 
-const machine1 = new CoffeeMachine(10)
-const machine2 = new CoffeeMachine(20)
-console.log(machine1)
+// Prototype member level, 콘솔 자세히 들어가보면 나옴.
+CoffeeMachine.prototype.makeCoffee = (shots) => {
+  console.log("making... ☕️");
+};
+const machine1 = new CoffeeMachine(10);
+const machine2 = new CoffeeMachine(20);
+console.log(machine1);
 console.log(machine2);
 
-// 4분 20초
+function LatteMachine(milk) {
+  this.milk = milk;
+}
+LatteMachine.prototype = Object.create(CoffeeMachine.prototype);
+
+const latteMachine = new LatteMachine(123);
+console.log(latteMachine);
+latteMachine.makeCoffee();
