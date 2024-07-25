bcd93bde569e470b05293148ff113321141c4619
ParkJuhyun <wngus6429@gmail.com>
2024-07-26 03:02:13 +0900

    q

--- NextBlog/src/app/posts/page.tsx
+++ NextBlog/src/app/posts/page.tsx
@@ -3,14 +3,15 @@ import { getAllPosts } from "@/service/posts";
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
+  console.log(posts);
   console.log(
     "filter",
     posts.filter((post) => post.featured)
   );
   // map해서 featured의 값이 low 이면 true, 아니면 false 반환해서 배열 만듬
   console.log(
-    "posts",
-    posts.map((post) => post.featured === "low")
+    "map",
+    posts.map((post) => post.featured)
   );
   const categories = [...new Set(posts.map((post) => post.category))];
   console.log("categories", categories);
